7cced4ce3f2da77d3b6499a87684721b8ffd8bce
treefrog18 <keafz0829@gmail.com>
2020-10-05 14:29:01 +0900

    env

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 var express = require("express");
-
 var mongoose = require("mongoose");
+require("dotenv").config();
 
 var app = express();
 
 app.use(express.urlencoded({ extended: false }));
 
-mongoose.connect(
-  "mongodb+srv://root:1234@cluster0.9mw3g.mongodb.net/mydb?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+var mongo_url = process.env.MONGO_URL;
+mongoose.connect(mongo_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/", function (request, response) {
   console.log(request);
   response.send("Hello World!!!!!!");
 });
 
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 app.listen(port, function () {
   console.log(`server is Starting at http://localhost:${port}`);
