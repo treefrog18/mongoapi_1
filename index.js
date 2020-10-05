d9cc900e40f9138ee5f5b3d58090f5ea355ae710
treefrog18 <keafz0829@gmail.com>
2020-10-05 12:22:13 +0900

    mongoose step 1

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var express = require("express");
 
+var mongoose = require("mongoose");
+
 var app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+mongoose.connect(
+  "mongodb+srv://root:1234@cluster0.9mw3g.mongodb.net/mydb?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 app.get("/", function (request, response) {
   console.log(request);
-  response.send("Hello World");
+  response.send("Hello World!!!!!!");
 });
 
 var port = 8080;
